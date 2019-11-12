1c827845d2b0f517678547fd7aea7025c574ec12
Yumi-does-code <57684365+Yumi-does-code@users.noreply.github.com>
2019-11-12 20:39:28 +0100

    Add files via upload

---
+++ client/index.js
@@ -0,0 +1,421 @@
+
+window.docLoaded = new Promise((resolve, reject) => {
+	document.addEventListener("DOMContentLoaded", e => {
+		resolve();
+	});
+});
+
+
+(async $ => {
+	
+	window.getCookie = (name) => {
+	    function escape(s) { return s.replace(/([.*+?\^${}()|\[\]\/\\])/g, '\\$1'); };
+	    var match = document.cookie.match(RegExp('(?:^|;\\s*)' + escape(name) + '=([^;]*)'));
+	    return match ? match[1] : null;
+	}
+
+
+
+	(await window.docLoaded);
+
+	const f = {
+		user: Promise,
+		messages: {},
+		area: document
+			.querySelector(".ui .textarea"),
+		room: document
+			.querySelector(".room-content"),
+		channel: "General",
+		authLevel: 0,
+		channels: {
+			"General": {
+				auth: 0,
+				premium: false,
+				readonly: false
+			},
+			"Help/support": {
+				auth: 0,
+				premium: false,
+				readonly: false
+			},
+			"Vip": {
+				auth: 0,
+				premium: true,
+				readonly: false
+			},
+			"Staff": {
+				auth: 1,
+				premium: false,
+				readonly: false
+			},
+			"Admin": {
+				auth: 4,
+				premium: false,
+				readonly: false
+			},
+			"Misbehaviour": {
+				auth: 1,
+				premium: false,
+				readonly: true
+			},
+			"Audit Logs": {
+				auth: 1,
+				premium: false,
+				readonly: true
+			},
+		},
+		switchChannel: channel => {
+			f.fixArea(channel);
+			f.messages = {};
+			f.clearRoom();
+			f.socket.emit("switchChannel", channel);
+
+
+			f.channel = channel;
+			if (document
+				.querySelector(`.active-menu-item`))
+				document
+					.querySelector(`.active-menu-item`)
+				.classList
+				.remove("active-menu-item");
+			document
+				.querySelector(`.chat-menu-item[xid='${f.slug(f.channel)}']`)
+				.classList
+				.add("active-menu-item")
+
+			document
+				.body
+				.classList
+				.remove("channels-menu-open")
+		},
+		sendMessage: e => {
+			e.preventDefault();
+
+			const val = f.area.innerHTML
+				.replace(/\<br\>|\<br\\\>/gi, "%BR_OBJECT%");
+			const token = getCookie("token");
+			const id = getCookie("user_id");
+
+			if (f.auth === false) {
+				window.location.href = "https://animedelta.net/@/login/?red=" + encodeURI(window.location.href);
+				retunr;
+			}
+			if (f.auth === undefined) {
+				setStatus({
+					show: true,
+					message: "Please wait until we've verified your account."
+				});
+				return;
+			}
+
+			if (val.length > 2000) {
+				setStatus({
+					show: true,
+					message: "You cannot send more than 2000 characters."
+				});
+ 
+				return;
+			}
+			val && f.socket &&
+			f.socket.emit("message", {
+				channel: f.channel,
+				text: val
+			}) && (f.area.innerHTML = "");
+		},
+		socket: io(`ws://localhost:80/`),
+		input: e => {
+			const which = e.which || e.keyCode;
+			const shift = e.shiftKey || e.shift
+			// is enter and not shift enter ? 
+
+			if (which === 13 && !shift) {
+				f.sendMessage(e);
+			}
+
+			// is enter and shift
+			else if (which === 13 && shift) {
+
+			}
+		},
+		slug: t => {
+			return encodeURI(t);
+		},
+		deSlug: t => {
+			return decodeURI(t);
+		},
+		clearRoom: () => {
+			f
+				.room
+				.innerHTML = "";
+		},
+		receiveBulkMessages: messages => {
+			f.clearRoom();
+			displayMessage(messages);
+			f.toBottom();
+		},
+		addChannels: channels => {
+			Object.keys(channels)
+				.forEach((v, i) => {
+					if (f.authLevel >= channels[v].auth && !document.querySelector(`.chat-menu-item[xid='${f.slug(v)}']`))
+						document
+							.querySelector(".chat-menu-inner")
+							.insertAdjacentHTML("beforeend", `
+								<div style="--it: ${i * 5};--base: 43;" xid="${f.slug(v)}" class="chat-menu-item">
+									${v}
+								</div>
+								`)
+				});
+		},
+		fixArea: channel => {
+			const channelMeta = f.channels[channel];
+			if (!channelMeta)
+				return;
+			if (channelMeta.readonly) {
+				f.area.setAttribute("disabled", true);
+				f.area.setAttribute("placeholder", `${channel} is read only`);
+			} else {
+				f.area.removeAttribute("disabled");
+				f.area.setAttribute("placeholder", `Message in #${channel}`);
+			}
+
+		},
+		toBottom: () => {
+			f.room.scrollTop = f.room.getBoundingClientRect().height + 100
+		}
+	}
+	f.area.focus();
+	f.addChannels(f.channels);
+	
+	connectChannel(f.channel);
+
+	window.addEventListener("click", e => {
+		if (e.target.classList.contains("chat-menu-item")) {
+			const id = e.target.getAttribute("xid");
+			f.switchChannel(f.deSlug(id));
+		}
+	});
+
+
+
+
+	async function connectChannel(channel) {
+
+
+		const token = getCookie("token");
+		const id = getCookie("user_id");
+
+		const gateway = new Promise((resolve, reject) => {
+			f.socket.on("connect", socket => {
+				f.switchChannel(f.channel);
+				resolve(true);
+			});
+		});
+
+
+		f.socket.on("login", d => {
+			const token = getCookie("token");
+			const id = getCookie("user_id");
+			f.socket.emit("login", {
+				id: id,
+				token: token
+			});
+		});
+
+		f.socket.on("messages", messages => {
+			f.receiveBulkMessages(messages);
+			//f.switchChannel(f.channel);
+		});
+
+
+		f.socket.on("status", status => {
+			setStatus(status);
+		});
+
+		f.socket.on("unauthorizedd", id => {
+			f.auth = false;
+		});
+
+		f.socket.on("authorized", user => {
+			f.auth = true;
+			f.authLevel = user.auth;
+			f.user.resolve(user);
+			f.addChannels(f.channels);
+		});
+
+		f.socket.on("message", message => {
+			displayMessage([message]);
+			f.toBottom();
+		});
+	}
+
+	// hook message 
+
+	const form = document
+		.querySelector(".ui");
+
+	form
+		.addEventListener("submit", f.sendMessage);
+
+	form
+		.addEventListener("keydown", f.input);
+
+
+	// status
+
+	function setStatus(status) {
+		document
+			.body
+			.classList[(status.show?"add":"add")]("status-show");
+
+		document
+			.querySelector(".status .text")
+			.innerText = status.message;
+
+		window.statusTimer = setTimeout(() => {
+			document
+			.body
+			.classList.remove("status-show");
+		}, 3000);
+	}
+
+	function displayMessage(messages) {
+	messages.forEach((message, i) => {
+		!f.messages[message.channel] &&
+		(f.messages[message.channel] = []);
+
+		console.log(message);
+
+		const prev = f.messages
+			[message.channel]
+			.slice(0)
+			.shift();
+
+		const isBlock = prev ? prev.author.id !== message.author.id : true;
+		const gMessage = genMessage(message, isBlock);
+
+		f.messages[message.channel].unshift(message);
+
+		if (isBlock)
+			f.room.insertAdjacentHTML("beforeend", gMessage);
+		else
+			f
+				.room
+				.querySelector(`.block[author='${prev.author.id}']:last-child .messages`)
+				.insertAdjacentHTML("beforeend", gMessage);
+
+	});
+
+
+	window.genContext = (origin, user, admin) => {
+		const id = Date.now();
+		return {html: `
+			<div class="context-menu" xid="${id}" tabindex="-1">
+				<div class="context-menu-inner">
+					<div class="context-menu-item">Copy text</div>
+					<div class="context-menu-item">Report user</div>
+					<div class="context-menu-item">Copy user id</div>
+					<div class="context-menu-item">Ban user</div>
+					<div class="context-menu-item">Mute user</div>
+				</div>
+			</div>
+		`,
+		id: id,
+		origin: origin,
+		user: user,
+		admin: admin};
+	}
+
+
+
+	
+	window.messageContext = async e => {
+		const user = e.target.closest(".block").getAttribute("author");
+
+		const admin = await f.user;
+
+		const ctx = genContext(e.target, user, admin);
+
+		displayCtx(ctx);
+	}
+	window.displayCtx = (ctx) => {
+		const prev = document.querySelector(".context-menu");
+
+		prev && (prev.outerHTML = "");
+
+		document.body.insertAdjacentHTML("beforeend", ctx.html);
+
+		const added = document.querySelector(`.context-menu[xid='${ctx.id}']`);
+
+		added.focus();
+
+		const pos = ctx.origin.getBoundingClientRect();
+
+		const dim = added.getBoundingClientRect();
+
+		added.style.top = `${window.innerHeight - pos.top - dim.height <= dim.height ? window.innerHeight - dim.height : pos.top + dim.height}px`
+
+		added.style.left = pos.left - 5 - dim.width + "px";
+	
+		added.addEventListener("blur", e => {
+			e.target.outerHTML = "";
+		});
+	}
+
+
+}
+document.querySelector(".channel-menu-btn")
+		.addEventListener("click", e => {
+			document.body.classList.toggle("channels-menu-open");
+			
+		});
+})(jQuery);
+
+
+
+
+
+
+
+
+var md = markdownit("default", {
+	  linkify: true,
+	  breaks: true
+});
+
+function genMessage(message, isBlock) {
+
+	let text = message.message
+		
+
+	while (text.match(/%BR_OBJECT%$/)) {
+		text = text.substr(0, text.length - "%BR_OBJECT%".length);
+	}
+
+	text = md
+		.render(text)
+		.replace(new RegExp("%BR_OBJECT%", "g"), "<br>")
+		
+
+	const smsg = `<div class="message-wrapper"><div time="${message.id}" author="${message.author.id}" class="message-text">${text}</div><div class="message-context" onclick="messageContext(event)"><i class="fas fa-ellipsis-v"></i></div></div>`;
+	const block = `
+		<div class="block" time="${message.id}" author="${message.author.id}">
+			<div class="pfp-column">
+				<div class="pfp" style="background-image: url('${message.author.pfp || `https://animedelta.net/no-pfp/logo${message.author.id.toString().substr(-1)}.png?hi=Yumi`}')"></div>
+			</div>
+			<div class="block-meta">
+				<div class="block-title">
+					<span>${message.author.username}</span>
+				</div>
+				<div class="messages">
+					<div class="message">
+						${smsg}
+					</div>
+				</div>
+			</div>
+		</div>
+	`;
+
+	const msg = smsg;
+
+	return isBlock ? block : msg;
+}
---
+++ server/server.js
@@ -0,0 +1,273 @@
+const appIO = require('http').createServer(handler)
+const io = require('socket.io')(appIO);
+const express = require('express');
+const app = express();
+const port = 3000;
+const request = require('request');
+
+
+appIO.listen(80);
+
+function handler (req, res) {
+
+}
+
+const channels = {
+	"General": {
+		auth: 0,
+		premium: false,
+		readonly: false
+	},
+	"Help/support": {
+		auth: 0,
+		premium: false,
+		readonly: false
+	},
+	"Vip": {
+		auth: 0,
+		premium: true,
+		readonly: false
+	},
+	"Staff": {
+		auth: 1,
+		premium: false,
+		readonly: false
+	},
+	"Admin": {
+		auth: 4,
+		premium: false,
+		readonly: false
+	},
+	"Misbehaviour": {
+		auth: 1,
+		premium: false,
+		readonly: true
+	},
+	"Audit Logs": {
+		auth: 1,
+		premium: false,
+		readonly: true
+	},
+}
+
+const sockets = {};
+
+
+const messages = {}
+const channel = io;
+
+channel
+	.on("connect", async socket => {
+		// request client login token
+		let channelName = Object.entries(channels)[0][0];
+		socket.on("switchChannel", channel => {
+			Object.entries(channels)
+				.forEach((v, i) => {
+					socket.leave(v[0]);
+				});
+
+			if (channels[channel]) {
+				channelName = channel;
+				socket.join(channel);
+				socket.emit("messages", getMessages(100, channel));
+			}
+
+			
+		});
+		socket.emit("login", true);
+
+		// verify user and get user info
+
+		const user = await new Promise((resolve, reject) => {
+			socket.emit("status", {
+				show: true,
+				message: "Validating account information."
+			});
+			socket.on("login", async d => {
+				resolve(await getUserInfo(d.id, d.token));
+			});
+		});
+
+		// is user forged or valid? Is user not logged in?
+
+		if ((await user)) {
+			user["description"], user["friends"], user["anime_tracked"], user["comments"], user["comments"], user["anime_tracked"], user["watched"] = "irrelevant";
+
+			socket.emit("status", {
+				show: false,
+				message: `Account verified. Welcome back, ${user.username}`
+			});
+
+			
+
+			socket.emit("authorized", {
+				id: user.id,
+				auth: user.authentication_level
+
+			});
+
+
+			console.log(">>> " + user.username + " has connected.");
+			
+			sockets[socket.id] = user;
+
+			
+
+			socket.on("message", message => {
+				console.log(user.authentication_level, channels[channelName].auth, user.authentication_level >= channels[channelName].auth)
+
+				if (!user.banned
+					&& !user.tempBanned
+					&& !user.muted
+					&& message.text
+					&& message.text.length <= 2000
+					&& message.hasOwnProperty("channel")
+					&& channels.hasOwnProperty(message.channel)
+					&& channels[message.channel].readonly === false
+					&& user.authentication_level >= channels[channelName].auth
+					&& channels[channelName].premium ? user.premium : true)
+				{
+					if (user.authentication_level >= channels[channelName].auth) {
+						createMessage(user, channelName || "General", message);
+					}
+					
+				}
+			});
+			
+			
+
+
+
+		}
+		else {
+			unAuth(socket, user);
+		}
+
+		
+	});
+
+
+
+
+function getMessages(n, channelName) {
+	if (messages[channelName])
+		return messages[channelName].slice(-(n > 100 ? 100 : n));
+	return [];
+}
+
+
+function createMessage(user, channelName, message) {
+	const serializedMessage = serializeMessage(user, message.text);
+	const newMessage = {
+		author: user,
+		channel: channelName,
+		message: serializedMessage,
+		id: Date.now()
+	}
+	console.log(`<${user.username}>: ${serializedMessage}`);
+
+	if (typeof messages[channelName] !== "object")
+		messages[channelName] = [];
+	messages[channelName].push(newMessage);
+
+
+	io.in(channelName).emit("message", newMessage);
+}
+
+const allowedTags = [
+	{
+		tag: "||",
+		html: "spoiler"
+	}
+]
+
+function serializeMessage(user, text) {
+	if (user) {
+		if (user.authentication_level >= 4) {
+
+		}
+		else {
+			text = text
+				.replace(/\</gi, "&lt;")
+				.replace(/\>/gi, "&gt;")
+				.replace(/\\/gi, "&#92;");
+		}
+	}
+	else {
+		text = text
+			.replace(/\</gi, "&lt;")
+			.replace(/\>/gi, "&gt;")
+			.replace(/\\/gi, "&#92;");
+	}
+
+
+
+
+
+	return text;
+}
+
+
+
+
+
+
+
+
+function unAuth(socket, user) {
+	socket.emit("status", {
+		show: true,
+		message: "Failed to authenticate account information.",
+		unauth: true
+	});
+	socket.emit("unauthorizedd", user.id);
+
+	// USER INCORRECT DETAILS!!
+}
+
+
+
+
+
+function getUserInfo(user_id, token) {
+	
+	return new Promise((resolve, reject) => {
+		request.post('https://animedelta.net/@/verify.php', {
+			form: {
+				loginToken: token,
+				userId: user_id,
+				code: "87y83hndn338nddnof84hj34o9fh98hfdhfhw43fh984fhj4hf4hfojchj"
+			}
+			}, (error, res, body) => {
+				if (error) {
+					console.error(error)
+				return;
+			}
+
+			try {
+				resolve(JSON.parse(body));
+			} catch (e) {
+				resolve(false);
+			}
+		});
+	})
+	
+}
+
+
+
+
+
+
+
+app.get("/", (req, res) => {
+
+  res.render("../client/index.pug");
+
+});
+
+
+app.use(express.static('client'))
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
